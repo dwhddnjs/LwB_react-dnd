5cedf1672a9fadb7a7f212b0e341f445ef41de68
dwhddnjs <per1215@naver.com>
2021-09-01 01:45:53 +0900

    added homePage.js markup

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./HomePage.scss";
-import HomeHeader from "../UI/HomeHeader";
+import Header from "../UI/Header";
 
 function HomePage() {
   return (
-    <div>
-      <HomeHeader />
-    </div>
+    <body>
+      <Header />
+      <section>
+        <div>
+          <div>당신의 운동 루틴은 ?</div>
+          <p>
+            다양한 운동종목들을 마우스 드래그로 추가하여 삭제하여 본인에게 맞는
+            운동루틴을 계획 해보아요.
+          </p>
+          <button>Get Started</button>
+        </div>
+        <div>
+          <img src="" alt="뭐 대충 gif" />
+        </div>
+      </section>
+    </body>
   );
 }
 
---
+++ src/components/UI/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <div className="home-header">
+      <div className="home-title">LwB</div>
+    </div>
+  );
+}
+
+export default Header;
