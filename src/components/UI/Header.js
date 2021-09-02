fe49c0716bfe6573097d698c38733de645816b02
dwhddnjs <per1215@naver.com>
2021-09-03 02:17:57 +0900

    add logo

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./Header.scss";
 import logo from "../../img/logo.png";
 import icon from "../../img/icon.png";
+import arm from "../../img/arm.png";
 
 function Header() {
   return (
     <div className="home-header">
-      <img src={icon} alt="" />
+      <img src={arm} alt="" />
       <div className="home-title">LwB</div>
     </div>
   );
