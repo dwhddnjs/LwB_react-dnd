7dfb1a92b3864ab61e786a49e23fad7c8b8fc032
dwhddnjs <per1215@naver.com>
2021-09-04 02:33:40 +0900

    try to find api

--- src/components/PlanPage/PlanPage.js
+++ src/components/PlanPage/PlanPage.js
@@ -1,8 +1,24 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Header from "../UI/Header";
 import "./PlanPage.scss";
 
 function PlanPage() {
+  const API = process.env.REACT_APP_API_KEY;
+
+  const [item, setItem] = useState();
+
+  const workout = () => {
+    axios.get("https://wger.de/api/v2/exercise/").then((el) => {
+      console.log(el.data.results.slice(0, 3));
+      setItem(el.data.results);
+    });
+  };
+
+  useEffect(() => {
+    workout();
+  }, []);
+
   return (
     <div className="plan">
       <Header />
@@ -10,7 +26,9 @@ function PlanPage() {
         <section>
           <div className="list-container">
             <span>운동 종목</span>
-            <div className="list-content"></div>
+            <div className="list-content">
+              {item && item.map((el) => <div key={el.id}>{el.name}</div>)}
+            </div>
           </div>
 
           <div className="plan-container">
