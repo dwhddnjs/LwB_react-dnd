82d1df4d58160002e5ef75d615efeb46e4f3b9d7
dwhddnjs <per1215@naver.com>
2021-09-06 01:30:56 +0900

    almost finished dnd

--- src/Dnd.js
+++ src/Dnd.js
@@ -3,73 +3,137 @@ import { useEffect, useState } from "react";
 import Header from "./components/UI/Header";
 import PlanItem from "./components/UI/PlanItem";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import "./../src/components/PlanPage/PlanPage.scss";
+import { v4 as uuid } from "uuid";
+import arm from "./img/arm.png";
+import "./Dnd.scss";
+
+const itemsFromBackend = [
+  { id: uuid(), name: "벤치프레스", target: "가슴" },
+  { id: uuid(), name: "스쿼트", target: "하체" },
+  { id: uuid(), name: "데드리프트", target: "하체" },
+  { id: uuid(), name: "숄더프레스", target: "하체" },
+];
+
+const columnsFromBackend = {
+  [uuid()]: {
+    name: "운동 종목",
+    items: itemsFromBackend,
+  },
+  [uuid()]: {
+    name: "운동 계획",
+    items: [],
+  },
+};
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems,
+      },
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    });
+  }
+};
 
 function Dnd() {
-  const item = [
-    {
-      id: 1,
-      name: "벤치프레스",
-      target: "가슴",
-    },
-    {
-      id: 2,
-      name: "스쿼트",
-      target: "하체",
-    },
-    {
-      id: 3,
-      name: "데드리프트",
-      target: "등",
-    },
-    {
-      id: 4,
-      name: "숄더프레스",
-      target: "어깨",
-    },
-  ];
+  const [columns, setColumns] = useState(columnsFromBackend);
 
   return (
-    <div className="plan">
+    <div className="plan-page">
       <Header />
-      <body>
-        <section>
-          <div className="list-container">
-            <span>운동 종목</span>
-            <DragDropContext>
-              <Droppable droppableId="item">
-                {(provided) => (
-                  <div
-                    className="list-content"
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                  >
-                    {item.map(({ id, name }, index) => (
-                      <Draggable key={id} draggableId={id} index={index}>
-                        {(provided) => (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.dragHandleProps}
-                            {...provided.draggableProps}
-                          >
-                            {name}
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
-                    {provided.placeholder}
-                  </div>
-                )}
-              </Droppable>
-            </DragDropContext>
-          </div>
-
-          <div className="plan-container">
-            <span>운동 계획</span>
-            <div className="plan-content"></div>
-          </div>
-        </section>
-      </body>
+      <div className="plan-body">
+        <DragDropContext
+          onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+        >
+          {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+              <div className="plan-list" key={columnId}>
+                <h2>{column.name}</h2>
+                <div className="plan-boxs">
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          className="plan-lists"
+                          style={{
+                            background: snapshot.isDraggingOver
+                              ? "lightblue"
+                              : "#fff",
+                          }}
+                        >
+                          {column.items.map((item, index) => {
+                            return (
+                              <Draggable
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}
+                              >
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div
+                                      className="plan-item"
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={{
+                                        userSelect: "none",
+                                        backgroundColor: snapshot.isDragging
+                                          ? "#32a89d"
+                                          : "#3bc6b7",
+                                        ...provided.draggableProps.style,
+                                      }}
+                                    >
+                                      <img src={arm} alt="" />
+                                      <div className="item-des">
+                                        <span>{item.name}</span>
+                                        <span>{item.target}</span>
+                                      </div>
+                                    </div>
+                                  );
+                                }}
+                              </Draggable>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </div>
+                      );
+                    }}
+                  </Droppable>
+                </div>
+              </div>
+            );
+          })}
+        </DragDropContext>
+      </div>
     </div>
   );
 }
