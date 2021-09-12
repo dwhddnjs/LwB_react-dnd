cdd6f92311a7ddf3f9c713aa8674faa5e0ab18bc
dwhddnjs <per1215@naver.com>
2021-09-12 23:55:49 +0900

    removed data

--- src/components/PlanPage/PlanPage.js
+++ src/components/PlanPage/PlanPage.js
@@ -12,9 +12,6 @@ const workOutList = [
   { id: uuid(), name: "스쿼트", target: "하체" },
   { id: uuid(), name: "데드리프트", target: "하체" },
   { id: uuid(), name: "숄더프레스", target: "하체" },
-  { id: uuid(), name: "사레레", target: "어깨" },
-  { id: uuid(), name: "바벨로우", target: "등" },
-  { id: uuid(), name: "벤레레", target: "어깨" },
 ];
 
 const workOutTitle = {
